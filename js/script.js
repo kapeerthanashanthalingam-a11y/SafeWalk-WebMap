64a1e84012cbba89ae9add6e10b86bb2b3af0033
kapeerthanashanthalingam-a11y <kapeerthanashanthalingam@gmail.com>
2026-06-26 09:54:04 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+var map = L.map('map').setView([6.9271,79.8612],9);
 
+L.tileLayer(
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+attribution:'© OpenStreetMap contributors'
+}
+).addTo(map);
